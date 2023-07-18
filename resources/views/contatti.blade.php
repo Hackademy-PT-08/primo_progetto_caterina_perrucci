ddb00811f9d5f99876ce460328923597dea8e987
Emanuele Trossarello <emanuele.trossarello@aulab.it>
2023-07-18 20:29:23 +0200

    prova

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid p-5 bg-info">
         <div class="row justify-content-center">
             <div class="col-12">
-                <h1 class="display-1 text-center">Contattaci</h1>
+                <h1 class="display-1 text-center">Contdasdasattaci</h1>
             </div>
         </div>
     </div>
