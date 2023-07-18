398ce4d8a55ac118f0b88e94536fd13fd60ca414
Claudio-Paradisi <claudio.paradisi94@gmail.com>
2023-07-18 20:01:19 +0200

    ahaha

--- resources/views/collaboratori.blade.php
+++ resources/views/collaboratori.blade.php
@@ -2,7 +2,7 @@
 <div class="container-fluid p-5 bg-info">
         <div class="row justify-content-center">
             <div class="col-12">
-                <h1 class="display-1 text-center">I Collaboratori</h1>
+                <h1 class="display-1 text-center">I Collaboratoriii</h1>
             </div>
         </div>
     </div>
